95e19a8e04eaa8bdc9413a1073daf2a1f2b8a8d6
Willian Souza <willian.souza@compasso.com.br>
2024-12-09 14:46:54 -0300

    fix: refactured tests

--- src/components/Header/test.tsx
+++ src/components/Header/test.tsx
@@ -1,91 +1,75 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { usePathname } from 'next/navigation';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header, { HeaderProps } from '.';
 
-import Header from '.';
-import NavItem from './nav-item';
+jest.mock('./google-analytics', () => ({
+  GoogleAnalytics: () => <div data-testid="google-analytics" />,
+}));
 
-describe('<Header />', () => {
-  it('renders navigation items', () => {
+jest.mock('./nav-item', () => {
+  const MockNavItem = ({ label, href }: { label: string; href: string }) => (
+    <a href={href} data-testid={`nav-item-${label}`}>
+      {label}
+    </a>
+  );
+
+  // Definindo a displayName
+  MockNavItem.displayName = 'NavItem';
+
+  return MockNavItem;
+});
+
+describe('Header component', () => {
+  const defaultNavItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Projects', href: '/projects' },
+  ];
+
+  it('renders the logo', () => {
     const { container } = render(<Header />);
-
-    const navItems = ['Home', 'Projects'];
-
-    navItems.forEach((item) => {
-      expect(screen.getByText(item)).toBeInTheDocument();
-    });
+    const logo = screen.getByAltText('Logo');
+    expect(logo).toBeInTheDocument();
+    expect(logo).toHaveAttribute('src', '/images/logo.svg');
 
     expect(container.firstChild).toMatchSnapshot();
   });
-  it('renders the logo', () => {
+
+  it('renders the Google Analytics component', () => {
     render(<Header />);
-
-    const logo = screen.getByAltText('Logo');
-    expect(logo).toBeInTheDocument();
+    const googleAnalytics = screen.getByTestId('google-analytics');
+    expect(googleAnalytics).toBeInTheDocument();
   });
 
-  it('renders the flag button', () => {
+  it('renders the navigation items by default', () => {
     render(<Header />);
-
-    const flag = screen.getByAltText('United States Flag');
-    expect(flag).toBeInTheDocument();
+    defaultNavItems.forEach(({ label }) => {
+      const navItem = screen.getByTestId(`nav-item-${label}`);
+      expect(navItem).toBeInTheDocument();
+      expect(navItem).toHaveTextContent(label);
+    });
   });
 
-  it('warns when flag button is clicked', () => {
-    const consoleWarnMock = jest
-      .spyOn(console, 'warn')
-      .mockImplementation(() => {});
-
-    render(<Header />);
-
-    const flagButton = screen.getByRole('button');
-
-    fireEvent.click(flagButton);
-
-    expect(consoleWarnMock).toHaveBeenCalledWith(
-      'Localization change functionality is no longer supported.',
-    );
-
-    consoleWarnMock.mockRestore();
-  });
-});
-
-// Mock da função usePathname do Next.js
-jest.mock('next/navigation', () => ({
-  usePathname: jest.fn(),
-}));
-
-describe('<NavItem />', () => {
-  it('renders the label and link correctly', () => {
-    // Mock para retornar um pathname qualquer
-    (usePathname as jest.Mock).mockReturnValue('/');
-
-    render(<NavItem label="Home" href="/" />);
-
-    const link = screen.getByRole('link', { name: /home/i });
-
-    expect(link).toBeInTheDocument();
-    expect(link).toHaveAttribute('href', '/');
+  it('renders custom navigation items when provided', () => {
+    const customNavItems: HeaderProps['navItems'] = [
+      { label: 'About', href: '/about' },
+      { label: 'Contact', href: '/contact' },
+    ];
+    render(<Header navItems={customNavItems} />);
+    customNavItems.forEach(({ label }) => {
+      const navItem = screen.getByTestId(`nav-item-${label}`);
+      expect(navItem).toBeInTheDocument();
+      expect(navItem).toHaveTextContent(label);
+    });
   });
 
-  it('applies active class when pathname matches href', () => {
-    // Mock para simular o pathname atual igual ao href
-    (usePathname as jest.Mock).mockReturnValue('/projects');
-
-    render(<NavItem label="Projects" href="/projects" />);
-
-    const link = screen.getByRole('link', { name: /projects/i });
-
-    expect(link).toHaveClass('text-gray-50');
-  });
-
-  it('does not apply active class when pathname does not match href', () => {
-    // Mock para simular o pathname atual diferente do href
-    (usePathname as jest.Mock).mockReturnValue('/about');
-
-    render(<NavItem label="Home" href="/" />);
-
-    const link = screen.getByRole('link', { name: /home/i });
-
-    expect(link).not.toHaveClass('text-gray-50');
+  it('does not render default navigation items when custom items are provided', () => {
+    const customNavItems: HeaderProps['navItems'] = [
+      { label: 'About', href: '/about' },
+    ];
+    render(<Header navItems={customNavItems} />);
+    defaultNavItems.forEach(({ label }) => {
+      const navItem = screen.queryByTestId(`nav-item-${label}`);
+      expect(navItem).not.toBeInTheDocument();
+    });
   });
 });
