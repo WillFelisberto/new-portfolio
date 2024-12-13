95ea187ef4495254709e31ae96ebf4c009b42955
Willian Souza <willian.souza@compasso.com.br>
2024-12-13 16:16:53 -0300

    fix: vercel plugin import

--- payload.config.ts
+++ payload.config.ts
@@ -13,23 +13,45 @@ const __dirname = path.resolve();
 import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob';
 import { nodemailerAdapter } from '@payloadcms/email-nodemailer';
 
+const plugins = [];
+
+// Condicional para habilitar o Blob Storage
+if (process.env.BLOB_READ_WRITE_TOKEN) {
+  plugins.push(
+    vercelBlobStorage({
+      enabled: true, // Optional, defaults to true
+      collections: {
+        [Media.slug]: true,
+        [Projects.slug]: true,
+        [Technologies.slug]: true,
+        [Education.slug]: true,
+        [WorkExperience.slug]: true,
+        [About.slug]: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN!,
+    }),
+  );
+}
+
 export default buildConfig({
   // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor(),
-
-  email: nodemailerAdapter({
-    defaultFromAddress: 'info@payloadcms.com',
-    defaultFromName: 'Payload',
-    // Nodemailer transportOptions
-    transportOptions: {
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+  ...(process.env.SMTP_HOST && {
+    email: nodemailerAdapter({
+      defaultFromAddress: 'info@payloadcms.com',
+      defaultFromName: 'Payload',
+      // Nodemailer transportOptions
+      transportOptions: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
       },
-    },
+    }),
   }),
+
   // Define and configure your collections in this array
   collections: [Projects, Media, Technologies, Education, WorkExperience],
   globals: [About],
@@ -45,22 +67,7 @@ export default buildConfig({
     outputFile: path.resolve(__dirname, 'src/app/(payload)/payload-types.ts'),
   },
 
-  plugins: [
-    vercelBlobStorage({
-      enabled: true, // Optional, defaults to true
-      // Specify which collections should use Vercel Blob
-      collections: {
-        [Media.slug]: true,
-        [Projects.slug]: true,
-        [Technologies.slug]: true,
-        [Education.slug]: true,
-        [WorkExperience.slug]: true,
-        [About.slug]: true,
-      },
-      // Token provided by Vercel once Blob storage is added to your Vercel project
-      token: process.env.BLOB_READ_WRITE_TOKEN!,
-    }),
-  ],
+  plugins,
   // If you want to resize images, crop, set focal point, etc.
   // make sure to install it and pass it to the config.
   // This is optional - if you don't need to do these things,
